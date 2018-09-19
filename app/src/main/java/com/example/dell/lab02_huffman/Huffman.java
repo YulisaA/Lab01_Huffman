b54fea2a5d90c00d14ad4690c88a74ec4ca0666b
yulyazurdia8 <yulyazurdia8@hotmail.com>
2018-09-19 16:19:50 -0600

    comp y desc

---
+++ app/src/main/java/com/example/dell/lab02_huffman/Huffman.java
@@ -0,0 +1,223 @@
+package com.example.dell.lab02_huffman;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.os.Environment;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+import android.util.LogPrinter;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.security.AccessControlContext;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+@TargetApi(Build.VERSION_CODES.N)
+public class Huffman {
+
+    static final boolean newTextBasedOnOldOne = false;
+
+    static PriorityQueue<Node> nodes = new PriorityQueue<>((o1, o2) -> (o1.value < o2.value) ? -1 : 1);
+    static TreeMap<Character, String> codes = new TreeMap<>();
+    static String text = "";
+    static String encoded = "";
+    static String decoded = "";
+    static int ASCII[] = new int[128];
+
+    public static String Encode (String mytext)
+    {
+        text = mytext;
+        Scanner scanner = new Scanner(text);
+        text = scanner.nextLine();
+        ASCII = new int[128];
+        nodes.clear();
+        codes.clear();
+        encoded = "";
+        decoded = "";
+        System.out.println("Text: " + text);
+        calculateCharIntervals(nodes, true);
+        buildTree(nodes);
+        generateCodes(nodes.peek(), "");
+
+        String tree = printCodes();
+        System.out.println("-- Encoding/Decoding --");
+        encodeText();
+
+        return printCodes() + encodeText();
+    }
+    public String Decode(String o) {
+
+        decodeText(o);
+        return decodeText(o);
+    }
+
+
+    private static boolean IsSameCharacterSet() {
+        boolean flag = true;
+        for (int i = 0; i < text.length(); i++)
+            if (ASCII[text.charAt(i)] == 0) {
+                flag = false;
+                break;
+            }
+        return flag;
+    }
+
+    private static String decodeTextf() {
+        decoded = "";
+        Node node = nodes.peek();
+        for (int i = 0; i < encoded.length(); ) {
+            Node tmpNode = node;
+            while (tmpNode.left != null && tmpNode.right != null && i < encoded.length()) {
+                if (encoded.charAt(i) == '1')
+                    tmpNode = tmpNode.right;
+                else tmpNode = tmpNode.left;
+                i++;
+            }
+            if (tmpNode != null)
+                if (tmpNode.character.length() == 1)
+                    decoded += tmpNode.character;
+                else
+                    System.out.println("Input not Valid");
+
+        }
+        System.out.println("Decoded Text: " + decoded);
+        return decoded;
+    }
+    private String decodeText(String Textdesc) {
+        decoded = "";
+        TreeMap<String, String> myCodes = new TreeMap<>();
+        String[] parts = Textdesc.split(";");
+
+        for (int i = 0; i < parts.length -1; i++){
+            String[] subparts = parts[i].split(":");
+            myCodes.put(subparts[1].trim(), subparts[0].trim());
+
+        }
+
+        String cod = parts[parts.length - 1];
+        StringBuilder sb = new StringBuilder();
+        String []aux = new String[(cod.length())/8];
+
+
+           int i = 0;
+                for(int k = 0; k < cod.length();  k+=8)
+                {
+                aux[i] = cod.substring(k, k+8);
+                i++;
+            }
+
+        System.out.println(aux[2]);
+
+        for(int j = 0; j < cod.length()/8; j++)
+        {
+            sb.append(myCodes.get(aux[j]));
+        }
+        return sb.toString();
+    }
+
+    private static String encodeText() {
+        encoded = "";
+        for (int i = 0; i < text.length(); i++)
+            encoded += convert(codes.get(text.charAt(i)));
+        System.out.println("Encoded Text: " + encoded);
+
+        return encoded;
+    }
+
+    private static void buildTree(PriorityQueue<Node> vector) {
+        while (vector.size() > 1)
+            vector.add(new Node(vector.poll(), vector.poll()));
+    }
+
+    private static String printCodes() {
+        StringBuilder stringBuilder = new StringBuilder();
+        System.out.println("--- Printing Codes ---");
+        codes.forEach((k, v) -> System.out.println("'" + k + "' : " + v));
+
+
+        codes.forEach((k, v) -> stringBuilder.append(k +":" + convert(v) +";"+ "\n"));
+        return stringBuilder.toString();
+    }
+
+    private static String convert(String k)
+    {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(k);
+        for(int i = 0; sb.length() < 8; i++)
+        {
+            sb.append("0").toString();
+        }
+        return sb.toString();
+    }
+
+    private static void calculateCharIntervals(PriorityQueue<Node> vector, boolean printIntervals) {
+        if (printIntervals) System.out.println("-- intervals --");
+
+        for (int i = 0; i < text.length(); i++)
+            ASCII[text.charAt(i)]++;
+
+        for (int i = 0; i < ASCII.length; i++)
+            if (ASCII[i] > 0) {
+                vector.add(new Node(ASCII[i] / (text.length() * 1.0), ((char) i) + ""));
+                if (printIntervals)
+                    System.out.println("'" + ((char) i) + "' : " + ASCII[i] / (text.length() * 1.0));
+            }
+    }
+
+    private static void generateCodes(Node node, String s) {
+        if (node != null) {
+            if (node.right != null)
+                generateCodes(node.right, s + "1");
+
+            if (node.left != null)
+                generateCodes(node.left, s + "0");
+
+            if (node.left == null && node.right == null)
+            {
+                codes.put((node.character.charAt(0)), s);
+            }
+
+        }
+    }
+}
+
+class Node {
+    Node left, right;
+    double value;
+    String character;
+
+    public Node(double value, String character) {
+        this.value = value;
+        this.character = character;
+        left = null;
+        right = null;
+    }
+
+    public Node(Node left, Node right) {
+        this.value = left.value + right.value;
+        character = left.character + right.character;
+        if (left.value < right.value) {
+            this.right = right;
+            this.left = left;
+        } else {
+            this.right = left;
+            this.left = right;
+        }
+    }
+}
+
--- app/src/main/java/com/example/dell/lab02_huffman/MainActivity.java
+++ app/src/main/java/com/example/dell/lab02_huffman/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.dell.lab02_huffman;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btnComp)
+    Button btnComp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnComp)
+    public void onViewClicked() {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
