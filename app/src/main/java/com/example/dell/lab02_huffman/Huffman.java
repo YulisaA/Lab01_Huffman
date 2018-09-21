f530552e3537be353f302e0a7bf81540265b487b
yulyazurdia8 <yulyazurdia8@hotmail.com>
2018-09-20 23:56:22 -0600

    last

--- app/src/main/java/com/example/dell/lab02_huffman/Huffman.java
+++ app/src/main/java/com/example/dell/lab02_huffman/Huffman.java
@@ -18,7 +18,6 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.PriorityQueue;
--- app/src/main/java/com/example/dell/lab02_huffman/MainActivity2.java
+++ app/src/main/java/com/example/dell/lab02_huffman/MainActivity2.java
@@ -65,7 +65,6 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         ButterKnife.bind(this);
-
         Lista = new ArrayList<String>();
 
         adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.scheme, Lista);
